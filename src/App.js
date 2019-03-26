e580a8fbd8c36a5dcaf85843c69219cfad9eb85f
luke <lukekennedy212@gmail.com>
2019-03-26 21:05:04 +0800

    rental card

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import { Header } from './common/header/Header'
+import { RentalCard } from './components/rental/RentalCard'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <Header />
-        I am app COMPONENT
+        <Header />
+        <div className='container'>
+          <section id='rentalListing'>
+            <h1 class='page-title'>Your Home All Around the World</h1>
+            <div class='row'>
+              <RentalCard />
+              <RentalCard />
+              <RentalCard />
+            </div>
+          </section>
+        </div>
     </div>
     );
   }
